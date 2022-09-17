f9d695cc62c8cfdfdea5e520753e30d444454b5b
Joshy Wilson <joshjwwilson@gmail.com>
2022-09-17 09:04:03 +0100

    create DVD class and add properties, getters and setters.

--- src/dto/DVD.java
+++ src/dto/DVD.java
@@ -2,4 +2,58 @@ package dto;
 
 public class DVD {
 	
+	private String title;
+	private String releaseDate;
+	private String mpaaRating;
+	private String director;
+	private String studio;
+	private String note;
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	public String getRating() {
+		return mpaaRating;
+	}
+	
+	public void setRating(String rating) {
+		this.mpaaRating = rating;
+	}
+	
+	public String getDirector() {
+		return director;
+	}
+	
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	
+	public String getStudio() {
+		return studio;
+	}
+	
+	public void setStudio(String studio) {
+		this.studio = studio;
+	}
+	
+	public String getNote() {
+		return note;
+	}
+	
+	public void setNote(String note) {
+		this.note = note;
+	}
 }
